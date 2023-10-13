af115e59f02417d03ec6e36ee140c1346f6aa0c9
phil <phil.aldridge@bjss.com>
2023-10-13 16:30:26 +0100

    Added authentication on write page

--- pages/write/index.js
+++ pages/write/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head';
-import dynamic from 'next/dynamic'
 import { useState, useEffect } from 'react';
 import CKeditor from '../../components/CKeditor';
 import styles from './write.module.css'
 import Layout from '../../components/layout';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import awsExports from '../../src/aws-exports';
+Amplify.configure(awsExports);
 
-const MDEditor = dynamic(
-    () => import("@uiw/react-md-editor").then((mod) => mod.default),
-    { ssr: false }
-  );
 export default function Write() {
     const [title,setTitle] = useState("");
     const [id,setId] = useState('');
@@ -24,20 +24,22 @@ export default function Write() {
       <Head>
         <title>Write a new blog post here</title>
       </Head>
-      <h1>Write a new blog post</h1>
-      <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className={styles.titleInput} placeholder='Type your title here'/>
-      <input type="text" value={id} onChange={(e)=>setId(e.target.value)} className={styles.titleInput} placeholder='set a unique filename'/>
-      <button name="post" onClick={()=>createPost(data)} className={styles.postButton}>Post</button>
-      <div style={{minHeight:"600px"}}>
-        <CKeditor
-          name="description"
-          onChange={(data) => {
-            setData(data);
-          }}
-          editorLoaded={editorLoaded}
-        />
-         {JSON.stringify(data)}
-      </div>
+        <Authenticator LoginMechanisms={['email']} hideSignUp={true}>
+        <h1>Write a new blog post</h1>
+        <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className={styles.titleInput} placeholder='Type your title here'/>
+        <input type="text" value={id} onChange={(e)=>setId(e.target.value)} className={styles.titleInput} placeholder='set a unique filename'/>
+        <button name="post" onClick={()=>createPost(data)} className={styles.postButton}>Post</button>
+        <div>
+          <CKeditor
+            name="description"
+            onChange={(data) => {
+              setData(data);
+            }}
+            editorLoaded={editorLoaded}
+          />
+          {JSON.stringify(data)}
+        </div>
+        </Authenticator>
     </Layout>
   );
 }
