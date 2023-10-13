f5daa4040f77ec74c7ef8f612990b10037ac9ca8
phil <phil.aldridge@bjss.com>
2023-10-13 14:42:07 +0100

    Using CKEditor

--- pages/write/index.js
+++ pages/write/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import dynamic from 'next/dynamic'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import CKeditor from '../../components/CKeditor';
 import styles from './write.module.css'
 import Layout from '../../components/layout';
 
@@ -9,16 +10,14 @@ const MDEditor = dynamic(
     { ssr: false }
   );
 export default function Write() {
-    const [value, setValue] = useState("**Hello world!!!**");
     const [title,setTitle] = useState("");
     const [id,setId] = useState('');
+    const [editorLoaded, setEditorLoaded] = useState(false);
+    const [data, setData] = useState("");
 
-    const combinedMdString = `---
-title: '` + title + `'
-id: ` + id +`
-date: '`+ (new Date).toISOString().split('T')[0] +`'
----
-` + value;
+    useEffect(() => {
+      setEditorLoaded(true);
+    }, []);
 
   return (
     <Layout>
@@ -26,26 +25,28 @@ date: '`+ (new Date).toISOString().split('T')[0] +`'
         <title>Write a new blog post here</title>
       </Head>
       <h1>Write a new blog post</h1>
-      <h3>Write your post on the left side and see what the formatting will look like on the right</h3>
       <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className={styles.titleInput} placeholder='Type your title here'/>
       <input type="text" value={id} onChange={(e)=>setId(e.target.value)} className={styles.titleInput} placeholder='set a unique filename'/>
-      <button name="post" onClick={()=>createPost(combinedMdString)} className={styles.postButton}>Post</button>
-      <div className="container">
-        <MDEditor
-            value={value}
-            onChange={setValue}
-            className={styles.mdEditor}
+      <button name="post" onClick={()=>createPost(data)} className={styles.postButton}>Post</button>
+      <div style={{minHeight:"600px"}}>
+        <CKeditor
+          name="description"
+          onChange={(data) => {
+            setData(data);
+          }}
+          editorLoaded={editorLoaded}
         />
+         {JSON.stringify(data)}
       </div>
     </Layout>
   );
 }
 
-async function createPost(combinedMdString) {
-  console.log(combinedMdString)
+async function createPost(data) {
+  console.log(data)
   const res = await fetch('../api/write', {
     method: 'POST',
-    body: combinedMdString
+    body: data
   })
 
   if (!res.ok) console.error(await res.text())
